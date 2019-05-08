e75afebb6f4fa0db9b5cb88f69fa36185054ad98
Jubi Taneja <jubi@cs.utah.edu>
2019-05-08 16:12:42 -0600

    add dummy solver.cpp

---
+++ third_party/llvm/lib/Analysis/Solver.cpp
@@ -0,0 +1,43 @@
+
+#include "llvm/ADT/APInt.h"
+#include "llvm/ADT/SetVector.h"
+#include "llvm/ADT/Statistic.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ErrorHandling.h"
+#include "llvm/Support/KnownBits.h"
+
+#include <unordered_map>
+
+using namespace llvm;
+
+namespace {
+
+static cl::opt<bool> NoInfer("souper-no-infer",
+    cl::desc("Populate the external cache, but don't infer replacements (default=false)"),
+    cl::init(false));
+static cl::opt<bool> InferNop("souper-infer-nop",
+    cl::desc("Infer that the output is the same as an input value (default=false)"),
+    cl::init(false));
+static cl::opt<bool> StressNop("souper-stress-nop",
+    cl::desc("stress-test big queries in nop synthesis by always performing all of the small queries (slow!) (default=false)"),
+    cl::init(false));
+static cl::opt<int>MaxNops("souper-max-nops",
+    cl::desc("maximum number of values from the LHS to try to use as the RHS (default=20)"),
+    cl::init(20));
+static cl::opt<bool> InferInts("souper-infer-iN",
+    cl::desc("Infer iN integers for N>1 (default=true)"),
+    cl::init(true));
+static cl::opt<bool> InferInsts("souper-infer-inst",
+    cl::desc("Infer instructions (default=false)"),
+    cl::init(false));
+static cl::opt<bool> EnableExhaustiveSynthesis("souper-exhaustive-synthesis",
+    cl::desc("Use exaustive search for instruction synthesis (default=false)"),
+    cl::init(false));
+static cl::opt<int> MaxLHSSize("souper-max-lhs-size",
+    cl::desc("Max size of LHS (in bytes) to put in external cache (default=1024)"),
+    cl::init(1024));
+
+
+}
+
