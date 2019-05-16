82105a7d8251d74e49b2feb76409b29fe7bf843d
Jubi Taneja <jubi@cs.utah.edu>
2019-05-15 19:05:04 -0600

    missing function from souper's pass.cpp

--- third_party/llvm/lib/Analysis/ValueTracking.cpp
+++ third_party/llvm/lib/Analysis/ValueTracking.cpp
@@ -13,6 +13,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Analysis/ValueTracking.h"
+#include "llvm/Analysis/Passes.h"
 #include "llvm/ADT/APFloat.h"
 #include "llvm/ADT/APInt.h"
 #include "llvm/ADT/ArrayRef.h"
@@ -32,6 +33,17 @@
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/OptimizationRemarkEmitter.h"
 #include "llvm/Analysis/TargetLibraryInfo.h"
+
+#include "llvm/Analysis/Candidates.h"
+#include "llvm/Analysis/Solver.h"
+#include "llvm/Analysis/KVStore.h"
+#include "llvm/Analysis/Parser.h"
+#include "llvm/Analysis/ConstantSynthesis.h"
+#include "llvm/Analysis/Inst.h"
+#include "llvm/Analysis/DemandedBits.h"
+#include "llvm/Analysis/LazyValueInfo.h"
+#include "llvm/Analysis/ScalarEvolution.h"
+
 #include "llvm/IR/Argument.h"
 #include "llvm/IR/Attributes.h"
 #include "llvm/IR/BasicBlock.h"
@@ -61,12 +73,16 @@
 #include "llvm/IR/Type.h"
 #include "llvm/IR/User.h"
 #include "llvm/IR/Value.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LegacyPassManager.h"
+
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Compiler.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/KnownBits.h"
 #include "llvm/Support/MathExtras.h"
+#include "llvm/Support/MemoryBuffer.h"
 #include <algorithm>
 #include <array>
 #include <cassert>
@@ -74,22 +90,19 @@
 #include <iterator>
 #include <utility>
 
-#include "llvm/Analysis/Candidates.h"
-#include "llvm/Analysis/Solver.h"
-#include "llvm/Analysis/KVStore.h"
-#include "llvm/Analysis/Parser.h"
-#include "llvm/Analysis/ConstantSynthesis.h"
-#include "llvm/Analysis/Inst.h"
-#include "llvm/Support/MemoryBuffer.h"
-#include "llvm/Analysis/DemandedBits.h"
-#include "llvm/Analysis/LazyValueInfo.h"
-#include "llvm/Analysis/ScalarEvolution.h"
-#include "llvm/IR/IRBuilder.h"
 #include "llvm/Pass.h"
-#include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Analysis/SMTLIBSolver.h"
+#include "llvm/Analysis/GetSolverFromArgs.h"
+#include "llvm/Analysis/CandidateMapUtils.h"
+#include "llvm/ADT/Statistic.h"
+
+
+STATISTIC(InstructionReplaced, "Number of instructions replaced by another instruction");
+STATISTIC(DominanceCheckFailed, "Number of failed replacement due to dominance check");
 
 using namespace llvm;
 using namespace llvm::PatternMatch;
+using namespace souper;
 
 const unsigned MaxDepth = 6;
 
@@ -97,6 +110,33 @@ const unsigned MaxDepth = 6;
 // dominating comparisons.
 static cl::opt<unsigned> DomConditionsMaxUses("dom-conditions-max-uses",
                                               cl::Hidden, cl::init(20));
+
+static cl::opt<unsigned> DebugLevel("souper-debug-level", cl::Hidden,
+     cl::init(1),
+     cl::desc("Control the verbose level of debug output (default=1). "
+     "The larger the number is, the more fine-grained debug "
+     "information will be printed."));
+
+static cl::opt<bool> DynamicProfile("souper-dynamic-profile", cl::init(false),
+    cl::desc("Dynamic profiling of Souper optimizations (default=false)"));
+
+static cl::opt<bool> StaticProfile("souper-static-profile", cl::init(false),
+    cl::desc("Static profiling of Souper optimizations (default=false)"));
+
+static cl::opt<unsigned> FirstReplace("souper-first-opt", cl::Hidden,
+    cl::init(0),
+    cl::desc("First Souper optimization to perform (default=0)"));
+
+static cl::opt<unsigned> LastReplace("souper-last-opt", cl::Hidden,
+    cl::init(std::numeric_limits<unsigned>::max()),
+    cl::desc("Last Souper optimization to perform (default=infinite)"));
+
+#ifdef DYNAMIC_PROFILE_ALL
+static const bool DynamicProfileAll = true;
+#else
+static const bool DynamicProfileAll = false;
+#endif
+
 
 /// Returns the bitwidth of the given scalar or pointer type. For vector types,
 /// returns the element type's bitwidth.
@@ -5461,6 +5501,191 @@ Optional<bool> llvm::isImpliedByDomCondition(const Value *Cond,
   return isImpliedCondition(PredCond, Cond, DL, CondIsTrue);
 }
 
+Value *getValue(Inst *I, Instruction *ReplacedInst,
+                ExprBuilderContext &EBC, DominatorTree &DT,
+                std::map<Inst *, Value *> &ReplacedValues,
+                IRBuilder<> &Builder, Module *M) {
+  Type *T = Type::getIntNTy(ReplacedInst->getContext(), I->Width);
+  if (I->K == Inst::Const)
+    return ConstantInt::get(T, I->Val);
+
+  if (ReplacedValues.find(I) != ReplacedValues.end())
+    return ReplacedValues[I];
+
+  if (I->Origins.size() > 0) {
+    // if there's an Origin, we're connecting to existing code
+    for (auto V : I->Origins) {
+      if (V->getType() != T)
+        continue; // TODO: can we assert this doesn't happen?
+      if (isa<Argument>(V) || isa<Constant>(V))
+        return V;
+      if (auto IP = dyn_cast<Instruction>(V)) {
+        if (DT.dominates(IP, ReplacedInst)) {
+          ++InstructionReplaced;
+          return V;
+        } else {
+          ++DominanceCheckFailed;
+        }
+      } else {
+        report_fatal_error("Unhandled LLVM instruction in getValue()");
+      }
+    }
+    return 0;
+  }
+
+  // otherwise, recursively generate code
+  Value *V0 = getValue(I->Ops[0], ReplacedInst, EBC, DT, ReplacedValues,
+                       Builder, M);
+  if (!V0)
+    return 0;
+  switch (I->Ops.size()) {
+  case 1:
+    switch (I->K) {
+    case Inst::SExt:
+      return Builder.CreateSExt(V0, T);
+    case Inst::ZExt:
+      return Builder.CreateZExt(V0, T);
+    case Inst::Trunc:
+      return Builder.CreateTrunc(V0, T);
+    case Inst::CtPop:{
+      Function *F = Intrinsic::getDeclaration(M, Intrinsic::ctpop, T);
+      return Builder.CreateCall(F, V0);
+    }
+    case Inst::BSwap:{
+      Function *F = Intrinsic::getDeclaration(M, Intrinsic::bswap, T);
+      return Builder.CreateCall(F, V0);
+    }
+    case Inst::BitReverse:{
+      Function *F = Intrinsic::getDeclaration(M, Intrinsic::bitreverse, T);
+      return Builder.CreateCall(F, V0);
+    }
+    case Inst::Cttz:{
+      Function *F = Intrinsic::getDeclaration(M, Intrinsic::cttz, T);
+      // According to LLVM LangRef, the second argument of cttz i1 <is_zero_undef>
+      // must be a constant and is a flag to indicate whether the intrinsic should
+      // ensure that a zero as the first argument produces a defined result.
+      return Builder.CreateCall(F, {V0,
+            ConstantInt::get(V0->getContext(), APInt(1, 0))});
+    }
+    case Inst::Ctlz:{
+      // Ditto
+      Function *F = Intrinsic::getDeclaration(M, Intrinsic::ctlz, T);
+      return Builder.CreateCall(F, {V0,
+            ConstantInt::get(V0->getContext(), APInt(1, 0))});
+    }
+    default:
+      break;
+    }
+  case 2:{
+    Value *V1 = getValue(I->Ops[1], ReplacedInst, EBC, DT,
+                         ReplacedValues, Builder, M);
+    if (!V1)
+      return 0;
+    switch (I->K) {
+    case Inst::And:
+      if (isa<Constant>(V0))
+        return Builder.CreateAnd(V1, V0);
+      return Builder.CreateAnd(V0, V1);
+    case Inst::Or:
+      if (isa<Constant>(V0))
+        return Builder.CreateOr(V1, V0);
+      return Builder.CreateOr(V0, V1);
+    case Inst::Xor:
+      if (isa<Constant>(V0))
+        return Builder.CreateXor(V1, V0);
+      return Builder.CreateXor(V0, V1);
+    case Inst::Add:
+      if (isa<Constant>(V0))
+        return Builder.CreateAdd(V1, V0);
+      return Builder.CreateAdd(V0, V1);
+    case Inst::Sub:
+      return Builder.CreateSub(V0, V1);
+    case Inst::Mul:
+      if (isa<Constant>(V0))
+        return Builder.CreateMul(V1, V0);
+      return Builder.CreateMul(V0, V1);
+    case Inst::Shl:
+      return Builder.CreateShl(V0, V1);
+    case Inst::AShr:
+      return Builder.CreateAShr(V0, V1);
+    case Inst::LShr:
+      return Builder.CreateLShr(V0, V1);
+    case Inst::Ne:
+      if (isa<Constant>(V0)) {
+        return Builder.CreateICmpNE(V1, V0);
+      } else {
+        return Builder.CreateICmpNE(V0, V1);
+      }
+    case Inst::Eq:
+      if (isa<Constant>(V0)) {
+        return Builder.CreateICmpEQ(V1, V0);
+      } else {
+        return Builder.CreateICmpEQ(V0, V1);
+      }
+    case Inst::Ult:
+      if (isa<Constant>(V0)) {
+        return Builder.CreateICmpUGT(V1, V0);
+      } else {
+        return Builder.CreateICmpULT(V0, V1);
+      }
+    case Inst::Slt:
+      if (isa<Constant>(V0)) {
+        return Builder.CreateICmpSGT(V1, V0);
+      } else {
+        return Builder.CreateICmpSLT(V0, V1);
+      }
+    case Inst::Ule:
+      if (isa<Constant>(V0)) {
+        return Builder.CreateICmpUGE(V1, V0);
+      } else {
+        return Builder.CreateICmpULE(V0, V1);
+      }
+    case Inst::Sle:
+      if (isa<Constant>(V0)) {
+        return Builder.CreateICmpSGE(V1, V0);
+      } else {
+        return Builder.CreateICmpSLE(V0, V1);
+      }
+    case Inst::SAddSat:
+      return Builder.CreateCall(Intrinsic::getDeclaration(M, Intrinsic::sadd_sat, T), {V0, V1});
+    case Inst::UAddSat:
+      return Builder.CreateCall(Intrinsic::getDeclaration(M, Intrinsic::uadd_sat, T), {V0, V1});
+    case Inst::SSubSat:
+      return Builder.CreateCall(Intrinsic::getDeclaration(M, Intrinsic::ssub_sat, T), {V0, V1});
+    case Inst::USubSat:
+      return Builder.CreateCall(Intrinsic::getDeclaration(M, Intrinsic::usub_sat, T), {V0, V1});
+    default:
+      break;
+    }
+  }
+  case 3:{
+    Value *V1 = getValue(I->Ops[1], ReplacedInst, EBC, DT,
+                         ReplacedValues, Builder, M);
+    Value *V2 = getValue(I->Ops[2], ReplacedInst, EBC, DT,
+                         ReplacedValues, Builder, M);
+    if (!V1 || !V2)
+      return 0;
+    switch (I->K) {
+    case Inst::Select:
+      return Builder.CreateSelect(V0, V1, V2);
+    case Inst::FShl:
+    case Inst::FShr: {
+      Intrinsic::ID ID =
+          I->K == Inst::FShl ? Intrinsic::fshl : Intrinsic::fshr;
+      Function *F = Intrinsic::getDeclaration(M, ID, T);
+      return Builder.CreateCall(F, {V0, V1, V2});
+    }
+    default:
+      break;
+    }
+  }
+  default:
+    break;
+  }
+  report_fatal_error((std::string)"Unhandled Souper instruction " +
+                     Inst::getKindName(I->K) + " in getValue()");
+}
+
 bool runOnFunction(Function *F) {
   bool Changed = false;
   souper::InstContext IC;
@@ -5469,19 +5694,19 @@ bool runOnFunction(Function *F) {
 
   std::unique_ptr<souper::Solver> S;
   unsigned ReplacementIdx, ReplacementsDone;
-  KVStore *KV;
+  souper::KVStore *KV;
 
-  LoopInfo *LI = &getAnalysis<LoopInfoWrapperPass>(*F).getLoopInfo();
+  auto *LI = &getAnalysis<LoopInfoWrapperPass>().getLoopInfo();
   if (!LI)
     report_fatal_error("getLoopInfo() failed");
-  auto &DT = getAnalysis<DominatorTreeWrapperPass>(*F).getDomTree();
-  DemandedBits *DB = &getAnalysis<DemandedBitsWrapperPass>(*F).getDemandedBits();
+  auto &DT = getAnalysis<DominatorTreeWrapperPass>().getDomTree();
+  DemandedBits *DB = &getAnalysis<DemandedBitsWrapperPass>().getDemandedBits();
   if (!DB)
     report_fatal_error("getDemandedBits() failed");
-  LazyValueInfo *LVI = &getAnalysis<LazyValueInfoWrapperPass>(*F).getLVI();
+  LazyValueInfo *LVI = &getAnalysis<LazyValueInfoWrapperPass>().getLVI();
   if (!LVI)
     report_fatal_error("getLVI() failed");
-  ScalarEvolution *SE = &getAnalysis<ScalarEvolutionWrapperPass>(*F).getSE();
+  ScalarEvolution *SE = &getAnalysis<ScalarEvolutionWrapperPass>().getSE();
   if (!SE)
     report_fatal_error("getSE() failed");
   TargetLibraryInfo* TLI = &getAnalysis<TargetLibraryInfoWrapperPass>().getTLI();
@@ -5527,31 +5752,31 @@ bool runOnFunction(Function *F) {
 
   for (auto &Cand : CandMap) {
 
-    if (StaticProfile) {
-      std::string Str;
-      llvm::raw_string_ostream Loc(Str);
-      Cand.Origin->getDebugLoc().print(Loc);
-      std::string HField = "sprofile " + Loc.str();
-      ReplacementContext Context;
-      KV->hIncrBy(GetReplacementLHSString(Cand.BPCs, Cand.PCs,
-                                          Cand.Mapping.LHS,
-                                          Context), HField, 1);
-    }
-    if (DynamicProfileAll) {
-      dynamicProfile(F, Cand);
-      Changed = true;
-      continue;
-    }
-    if (std::error_code EC =
-        S->infer(Cand.BPCs, Cand.PCs, Cand.Mapping.LHS,
-                 Cand.Mapping.RHS, IC)) {
-      if (EC == std::errc::timed_out ||
-          EC == std::errc::value_too_large) {
-        continue;
-      } else {
-        report_fatal_error("Unable to query solver: " + EC.message() + "\n");
-      }
-    }
+//    if (StaticProfile) {
+//      std::string Str;
+//      llvm::raw_string_ostream Loc(Str);
+//      Cand.Origin->getDebugLoc().print(Loc);
+//      std::string HField = "sprofile " + Loc.str();
+//      ReplacementContext Context;
+//      KV->hIncrBy(GetReplacementLHSString(Cand.BPCs, Cand.PCs,
+//                                          Cand.Mapping.LHS,
+//                                          Context), HField, 1);
+//    }
+//    if (DynamicProfileAll) {
+//      dynamicProfile(F, Cand);
+//      Changed = true;
+//      continue;
+//    }
+//    if (std::error_code EC =
+//        S->infer(Cand.BPCs, Cand.PCs, Cand.Mapping.LHS,
+//                 Cand.Mapping.RHS, IC)) {
+//      if (EC == std::errc::timed_out ||
+//          EC == std::errc::value_too_large) {
+//        continue;
+//      } else {
+//        report_fatal_error("Unable to query solver: " + EC.message() + "\n");
+//      }
+//    }
     if (!Cand.Mapping.RHS)
       continue;
 
@@ -5613,8 +5838,8 @@ bool runOnFunction(Function *F) {
       errs() << "\"\n";
     }
 
-    if (DynamicProfile)
-      dynamicProfile(F, Cand);
+//    if (DynamicProfile)
+//      dynamicProfile(F, Cand);
 
     if (Cand.Mapping.LHS->HarvestKind == HarvestType::HarvestedFromDef) {
       I->replaceAllUsesWith(NewVal);
